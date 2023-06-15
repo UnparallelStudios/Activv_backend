abc538f8a2f670738ac317053e023e99f3f27896
jovanzac <jovanzac@gmail.com>
2023-06-15 13:39:49 +0530

    Implemented a basic backend for the scrapping application and implemented extraction of user profile

--- Scripts/login.py
+++ Scripts/login.py
@@ -1,48 +1,62 @@
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
-def getData(userDetails):
-    result = ""
-    with rq.Session() as s:
-        result = s.post("https://www.rajagiritech.ac.in/stud/ktu/student/varify.asp", data=userDetails)
-        result = s.get("https://www.rajagiritech.ac.in/stud/ktu/student/Leave.asp?code=2023S4AID")
 
-    soup = bs(result.content, "html.parser")
-    # print(soup.prettify())
+def login_n_scrape(login_details, url=False) :
+    """
+    Function that logs in a user and scrapes data from a mentioned url
+    If url is False or None, it simply logs the user in
+    """
+    with rq.Session() as session :
+            response = session.post("https://www.rajagiritech.ac.in/stud/ktu/student/varify.asp", login_details)
+            if url :
+                response = session.get(url)
+    
+    return response
+
+
+def extract_profile_details(login_details) :
+    response = login_n_scrape(login_details, "https://www.rajagiritech.ac.in/stud/ktu/Student/Home.asp")
+    
+    soup = bs(response.content, "html.parser")
+    for div in soup.find_all("div") :
+            try :
+                if div["class"] == ["scroller"] :
+                    text = div.text
+                    text = text[text.index(":")+1: -1].strip()
+                    
+                    return {
+                        "User_name": text,
+                        "User_image": ""
+                    }
+                    
+            except :
+                continue
+    
+    return False
+
+
+def get_attendence_details(login_details):
+    response = login_n_scrape(login_details, "https://www.rajagiritech.ac.in/stud/ktu/student/Leave.asp?code=2023S4AID")
+
+    soup = bs(response.content, "html.parser")
     dateData = soup.find_all('td', {"bgcolor": "#aaaaaa", "height": "35"})
-    # print(siblings)
+
     leaveData = dict()
-    user = userDetails["Userid"]
+    user = login_details["Userid"]
     leaveData["uid"] = user
     leaveData["leaves"] = list()
-    # print("**************************************")
-    # print(user)
+
     for date in dateData:
         siblings = date.find_next_siblings("td")
-        # print(date.string)
         dateString = date.string
         dateleaves = list()
         for i in range(0, 7):
             sibling = siblings[i]
             if sibling["bgcolor"] == "#9f0000":
-                # print(sibling.find("font").string)
                 leavedate = sibling.find("font").string
                 dateleaves.append(leavedate)
-            # print(sibling.find("font").string)
-        # print("**************************************")
-        leaveData["leaves"].append({dateString: dateleaves})
-    return leaveData
-"""
-{
-    "uid": "2108037",
-    "leaves": [
-        {
-            "24-May-2023": [101908/EN900E, ]
-        },
-         {
-            "20-May-2023": [101908/EN900E, ]
-        },
-    ]
-}
 
-"""
+        leaveData["leaves"].append({dateString: dateleaves})
+
+    return leaveData
--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,51 @@
-from flask import Flask, jsonify, request
+import requests as rq
+from bs4 import BeautifulSoup as bs
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
-from main import find_details
+from Scripts.login import get_attendence_details, extract_profile_details
+from flask_session import Session
+
 
 
 app = Flask(__name__)
 CORS(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home() :
     if request.method == "GET" :
-        return jsonify(find_details(uid="U2108002", psswd="210118"))
-    
+        login_details = {
+            "Userid": "U2108036",
+            "Password": "210145"
+        }
+        return jsonify(get_attendence_details(login_details))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login_pg() :
+    if request.method == "POST" :
+        login_details = request.json
+        user = extract_profile_details(login_details)
+        
+        # If extraction successfully worked then that means user details entered are correct
+        if not user :
+            return jsonify({
+                "Status": "Failure"
+            })
+        
+        else :
+            # Setting up the session data
+            session["User_name"] = " ".join([i.capitalize() for i in user["User_name"].split()])
+            session["User_image"] = user["User_image"]
+            
+            print(session["User_name"], session["User_image"])
+            return jsonify({
+                "Status": "Success"
+            })
+
+
 if __name__ == "__main__" :
     app.run(debug=True)
